bd6ecc8d52a25f1a72e873c5b3bb372312f2f150
Ryan <your_email@whatever.comclear>
2018-06-03 08:16:30 -0700

    small final edits

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
+let port = process.env.PORT || 8000;
+app.listen(port, function() {
+  console.log(`Running in localhost at port ${port}!`);
 });
